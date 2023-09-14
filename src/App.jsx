a8ade7b45aa8242e2e3382fb6bf7a2ba1fbfc23a
juanperez300 <juanperez4856@gmail.com>
2023-09-13 20:59:27 -0500

    Uploading Besnk-landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
+import Hero from "./Components/Hero";
+import Companies from "./Components/Companies";
+import Guide from "./Components/Guide";
+import Properties from "./Components/Properties";
+import Details from "./Components/Details";
+import GetStarted from "./Components/GetStarted";
+import Footer from "./Components/Footer";
 function App() {
-  return <></>;
+  return (
+    <>
+      <Hero/>
+      <Companies/>
+      <Guide/>
+      <Properties/>
+      <Details/>
+      <GetStarted/>
+      <Footer/>
+  </>
+  );
 }
 
 export default App;
--- src/Components/Companies.jsx
+++ src/Components/Companies.jsx
@@ -5,11 +5,61 @@ import starsImg from "../media/Star.png";
 import logosImg from "../media/logos.png";
 
 const Companies = () => {
-  const CustomContainer = styled(Container)(({ theme }) => ({}));
+  const CustomContainer = styled(Container)(({ theme }) => ({
+    display: "flex",
+    justifyContent: "space-between",
+    [theme.breakpoints.down("md")]: {
+      flexDirection: "column",
+      alignItems: "center",
+      textAlign: "center",
+      marginBottom: theme.spacing(4),
+    },
+  }));
 
-  const CustomBox = styled(Box)(({ theme }) => ({}));
+  const CustomBox = styled(Box)(({ theme }) => ({
+    [theme.breakpoints.down("md")]: {
+      marginBottom: theme.spacing(4),
+    },
+  }));
+  return (
+    <Box>
+      <CustomContainer>
+        <CustomBox>
+          <img src={logoImg} alt="logo" style={{ maxWidth: "100%" }} />
+          <Typography
+            variant="body2"
+            sx={{
+              color: "#7D8589",
+              fontSize: "16px",
+              fontWeight: "bold",
+              mt: 2,
+            }}
+          >
+            More than 45,000 trust Besnik
+          </Typography>
+        </CustomBox>
 
-  return <></>;
+        <Box>
+          <img src={starsImg} alt="stars" style={{ maxWidth: "100%" }} />
+          <Typography
+            variant="body2"
+            sx={{
+              color: "#7D8589",
+              fontSize: "16px",
+              fontWeight: "bold",
+              mt: 2,
+            }}
+          >
+            5-star Rating (2k+ Reviews)
+          </Typography>
+        </Box>
+      </CustomContainer>
+
+      <Container sx={{display: "flex", flexDirection: "column"}}>
+        <img src={logosImg} alt="logos"/>
+      </Container>
+    </Box>
+  );
 };
 
 export default Companies;
